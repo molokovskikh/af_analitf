69627b03e5a02c2dbfa68f8f308fe143e9dfd999
Morozov <Morozov@fc44fa57-7600-de43-8fc0-af1443fc480a>
2005-03-14 14:29:00 +0000

    Добавил работу с прогрессбаром и изменил алгоритм получения рекламы

--- src/RecThread.pas
+++ src/RecThread.pas
@@ -2,34 +2,60 @@ unit RecThread;
 
 interface
 
-uses Classes, SysUtils;
+uses Classes, SysUtils, IdException, WinSock, IdComponent, IdHTTP, Math, SOAPThroughHTTP;
 
 type
   TReclameThread = class(TThread)
-	Terminated: boolean;
+	 RecTerminated: boolean;
   private
     { Private declarations }
-  protected
+   FStatusPosition : Integer;
+   FStatusStr : String;
+   FSOAP : TSOAP;
+   procedure ClearProgress;
+   procedure UpdateProgress;
+   procedure HTTPReclameWork(Sender: TObject;
+    AWorkMode: TWorkMode;
+	  const AWorkCount: Integer);
+   procedure OnConnectError(AMessage : String);
+   protected
     procedure Execute; override;
   end;
 
 implementation
 
-uses Exchange, DModule, AProc;
+uses Exchange, DModule, AProc, VCLUnZip;
+
+procedure TReclameThread.ClearProgress;
+begin
+  FStatusPosition := 0;
+  FStatusStr := '';
+  ExchangeForm.lReclameStatus.Caption := '';
+  ExchangeForm.ReclameBar.Position := 0;
+end;
 
 procedure TReclameThread.Execute;
 var
 	FileStream: TFileStream;
 	ReclameURL: string;
 	RegionCode: string;
+  Res       : TStrings;
+//  TmpPos    : Integer;
+//  FileSize  : Integer;
+  NewReclame : Boolean;
+  ZipFileName : String;
+  UnZip       : TVCLUnZip;
 begin
-	Terminated := False;
+	RecTerminated := False;
+  Synchronize(ClearProgress);
 	RegionCode := DM.adtClients.FieldByName( 'RegionCode').AsString;
+
+{
 	ReclameURL := StringReplace( DM.adtReclame.FieldByName( 'ReclameURL').AsString, '#',
 		RegionCode, [rfReplaceAll, rfIgnoreCase]);
 	try
 		if ExchangeForm.HTTPReclame.Host = '' then exit;
-		ExchangeForm.HTTPReclame.Head( ReclameURL);
+ 		ExchangeForm.HTTPReclame.Head( ReclameURL);
 		if ExchangeForm.HTTPReclame.Response.LastModified <=
 			DM.adtReclame.FieldByName( 'UpdateDateTime').AsDateTime then
 		begin
@@ -42,7 +68,12 @@ begin
 	FileStream := TFileStream.Create( ExePath + SDirIn + '\r' + RegionCode + '.zip',
 		fmCreate or fmOpenWrite);
 	try
-		ExchangeForm.HTTPReclame.Get( ReclameURL, FileStream);
+    ExchangeForm.HTTPReclame.OnWork := HTTPReclameWork;
+    try
+      ExchangeForm.HTTPReclame.Get( ReclameURL, FileStream);
+    finally
+      ExchangeForm.HTTPReclame.OnWork := nil;
+    end;
 		DM.adtReclame.Edit;
 		DM.adtReclame.FieldByName( 'UpdateDateTime').AsDateTime := Now;
 		DM.adtReclame.Post;
@@ -55,7 +86,198 @@ begin
 	end
 	else
 		FileStream.Free;
-	Terminated := True;
+}
+
+
+
+  try
+{
+   	ReclameURL := 'http://' + ExtractURL( DM.adtParams.FieldByName( 'HTTPHost').AsString) +
+		'/' + DM.adtParams.FieldByName( 'ServiceName').AsString + '/code.asmx/GetReclame';
+ 		Res := ExchangeForm.HTTPReclame.Get(ReclameURL);
+    TmpPos := Pos('URL=', Res);
+    if TmpPos > 0 then begin
+      Res := Copy(Res, TmpPos + 4, Length(Res));
+      TmpPos := Pos(';Size=', Res);
+      if TmpPos > 5  then begin
+        ReclameURL := Copy(Res, 1, TmpPos-1);
+        Res := Copy(Res, TmpPos + 6, Length(Res));
+        FileSize := StrToIntDef(Res, 0);
+
+        if FileSize > 0 then begin
+          FileStream := TFileStream.Create( ExePath + SDirIn + '\r' + RegionCode + '.zip',
+            fmCreate or fmOpenWrite);
+          try
+
+            ExchangeForm.HTTPReclame.OnWork := HTTPReclameWork;
+            try
+              ExchangeForm.HTTPReclame.Get( ReclameURL, FileStream);
+            finally
+              ExchangeForm.HTTPReclame.OnWork := nil;
+            end;
+
+            DM.adtReclame.Edit;
+            DM.adtReclame.FieldByName( 'UpdateDateTime').AsDateTime := Now;
+            DM.adtReclame.Post;
+          except
+          end;
+          if FileStream.Size = 0 then
+          begin
+            FileStream.Free;
+            DeleteFile( ExePath + SDirIn + '\r' + RegionCode + '.zip');
+          end
+          else
+            FileStream.Free;
+        end;
+
+      end;
+    end;
+}
+
+   	ReclameURL := 'http://' + ExtractURL( DM.adtParams.FieldByName( 'HTTPHost').AsString) +
+		'/' + DM.adtParams.FieldByName( 'ServiceName').AsString + '/code.asmx';
+    FStatusStr := 'Запрос рекламы...';
+    Synchronize(UpdateProgress);
+    FSOAP := TSOAP.Create(ReclameURL, DM.adtParams.FieldByName( 'HTTPName').AsString,
+		  DM.adtParams.FieldByName( 'HTTPPass').AsString, OnConnectError, ExchangeForm.HTTPReclame);
+    try
+      Res := FSOAP.Invoke('GetReclame', [], []);
+      //Если в ответ что-то пришло, то скачиваем рекламу
+      if Length(Res.Text) > 0 then begin
+        ReclameURL := Res.Values['URL'];
+        NewReclame := StrToBoolDef(UpperCase(Res.Values['New']), True);
+
+        if Length(ReclameURL) > 0 then begin
+
+          ZipFileName := ExePath + SDirReclame + '\r' + RegionCode + '.zip';
+
+          if not NewReclame then
+          begin
+            if SysUtils.FileExists( ZipFileName) then
+              FileStream := TFileStream.Create( ZipFileName, fmOpenReadWrite)
+            else
+              FileStream := TFileStream.Create( ZipFileName, fmCreate);
+
+            if FileStream.Size > 1024 then
+              FileStream.Seek( -1024, soFromEnd)
+            else
+              FileStream.Seek( 0, soFromEnd);
+          end
+          else
+            FileStream := TFileStream.Create( ZipFileName, fmCreate);
+
+          try
+
+            if Terminated then Abort;
+            ExchangeForm.HTTPReclame.Request.ContentRangeStart := FileStream.Position;
+            ExchangeForm.HTTPReclame.OnWork := HTTPReclameWork;
+            try
+              ExchangeForm.HTTPReclame.Get( ReclameURL, FileStream);
+            finally
+              ExchangeForm.HTTPReclame.OnWork := nil;
+            end;
+
+          finally
+            FileStream.Free;
+          end;
+
+          if Terminated then Abort;
+          UnZip := TVCLUnZip.Create(nil);
+          try
+            UnZip.ZipName := ZipFileName;
+            UnZip.DestDir := ExePath + SDirReclame;
+            UnZip.UnZip;
+          finally
+            SysUtils.DeleteFile(ZipFileName);
+            UnZip.Free;
+          end;
+          
+          if Terminated then Abort;
+          FSOAP.Invoke('ReclameComplete', [], []);
+          
+          DM.adtReclame.Edit;
+          DM.adtReclame.FieldByName( 'UpdateDateTime').AsDateTime := Now;
+          DM.adtReclame.Post;
+
+        end;
+
+        FStatusStr := 'Загрузка рекламы завершена';
+        Synchronize(UpdateProgress);
+
+      end;
+
+    finally
+      FSOAP.Free;
+    end;
+
+  except
+  end;
+{
+}
+	RecTerminated := True;
+end;
+
+procedure TReclameThread.HTTPReclameWork(Sender: TObject;
+  AWorkMode: TWorkMode; const AWorkCount: Integer);
+var
+	Total, Current: real;
+	TSuffix, CSuffix: string;
+  HTTP : TIdHTTP;
+begin
+  if Terminated then Abort;
+
+  HTTP := TIdHTTP(Sender);
+	if HTTP.Response.ContentLength = 0 then FStatusPosition := 0
+	else
+	begin
+		FStatusPosition := Round(( HTTP.Response.ContentRangeStart+Cardinal(AWorkCount))/
+			( HTTP.Response.ContentRangeStart+Cardinal(HTTP.Response.ContentLength))*100);
+
+		TSuffix := 'Кб';
+		CSuffix := 'Кб';
+
+		Total := RoundTo(( HTTP.Response.ContentRangeStart +
+			Cardinal( HTTP.Response.ContentLength)) / 1024, -2);
+		Current := RoundTo((HTTP.Response.ContentRangeStart +
+			Cardinal( AWorkCount)) / 1024, -2);
+
+		if Total <> Current then
+		begin
+			if Total > 1000 then
+			begin
+				Total := RoundTo( Total / 1024, -2);
+				TSuffix := 'Мб';
+			end;
+			if Current > 1000 then
+			begin
+				Current := RoundTo( Current / 1024, -2);
+				CSuffix := 'Мб';
+			end;
+
+			if FStatusPosition > 0 then
+      begin
+//        ShowStatusText := True;
+//        stStatus.Caption := 'Загрузка данных   (' +
+        FStatusStr := 'Загрузка данных   (' +
+				FloatToStrF( Current, ffFixed, 10, 2) + ' ' + CSuffix + ' / ' +
+				FloatToStrF( Total, ffFixed, 10, 2) + ' ' + TSuffix + ')';
+//        WriteLn(LogFile, DateTimeToStr(Now) + '  HTTPWork : ' + FStatusStr);
+      end;
+		end;
+	end;
+
+  Synchronize(UpdateProgress);
+end;
+
+procedure TReclameThread.OnConnectError(AMessage: String);
+begin
+  if Terminated then Abort;
+end;
+
+procedure TReclameThread.UpdateProgress;
+begin
+  ExchangeForm.lReclameStatus.Caption := FStatusStr;
+  ExchangeForm.ReclameBar.Position := FStatusPosition;
 end;
 
 end.
