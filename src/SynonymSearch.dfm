49e43cfe4c73d39c40b05ce9d22f3ca3b50f31a5
Morozov <Morozov@fc44fa57-7600-de43-8fc0-af1443fc480a>
2006-07-06 09:28:00 +0000

    Форма поиска препаратов в синонимах.

---
+++ src/SynonymSearch.dfm
@@ -0,0 +1,6 @@
+inherited SynonymSearchForm: TSynonymSearchForm
+  Caption = 'SynonymSearchForm'
+  OldCreateOrder = True
+  PixelsPerInch = 96
+  TextHeight = 13
+end
---
+++ src/SynonymSearch.pas
@@ -0,0 +1,24 @@
+unit SynonymSearch;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Child;
+
+type
+  TSynonymSearchForm = class(TChildForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  SynonymSearchForm: TSynonymSearchForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
