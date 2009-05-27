96b0053390781781b93e1e3afc5eb75e4e5d5c6e
morozov <morozov@fc44fa57-7600-de43-8fc0-af1443fc480a>
2009-05-27 10:43:06 +0000

    #1559 Исчезает текущий клиент: забыл добавить эту форму в репозиторий.

---
+++ src/AnalitF/U_VistaCorrectForm.dfm
@@ -0,0 +1,16 @@
+object VistaCorrectForm: TVistaCorrectForm
+  Left = 333
+  Top = 201
+  Width = 651
+  Height = 334
+  Caption = 'VistaCorrectForm'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+end
---
+++ src/AnalitF/U_VistaCorrectForm.pas
@@ -0,0 +1,38 @@
+unit U_VistaCorrectForm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TVistaCorrectForm = class(TForm)
+  private
+    { Private declarations }
+  protected
+    procedure CreateParams(var Params: TCreateParams); override;
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.dfm}
+
+{ TVistaCorrectForm }
+
+procedure TVistaCorrectForm.CreateParams(var Params: TCreateParams);
+begin
+  inherited CreateParams(Params);
+  {
+    This only works on Windows XP and above
+    Ёто решение по скрытию компонентов при нажатии Alt при теме "Windows Vista"
+    ¬з€то отсюда: http://www.stevetrefethen.com/blog/UsingTheWSEXCOMPOSITEWindowStyleToEliminateFlickerOnWindowsXP.aspx
+    Ќа этой странице есть другие решени€ дл€ проблем под Vista: http://www.installationexcellence.com/articles/VistaWithDelphi/Index.html
+  }
+  if CheckWin32Version(5, 1) then
+    Params.ExStyle := Params.ExStyle or WS_EX_COMPOSITED;
+end;
+
+end.
