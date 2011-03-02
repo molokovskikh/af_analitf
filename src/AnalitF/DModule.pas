c0c0264683bee91ae39eacf2e0acc9d47d77a3db
Morozov Sergey <morozov@analit.net>
2011-03-02 08:35:24 +0300

    Добавил файлы для обновления программы с версии 1295 до версии 1353.
    Добавил ожидание при закрытии соединений.
    Обновил версию базы данных до 72.
    Полная версия программы 7.5.54.1353.

--- src/AnalitF/DModule.pas
+++ src/AnalitF/DModule.pas
@@ -4844,10 +4844,8 @@ begin
       try
         try
           ExportGlobalParamsFromOldLibMysqlDToFile(oldMySqlDB, PathToBackup, MySqlPathToBackup);
-{
           WriteExchangeLog('UpdateToNewLibMySqlDWithGlobalParams', 'Ждем после экспорта');
           Sleep(5000);
-}
         except
           on UpdateException : Exception do begin
             AProc.LogCriticalError('Ошибка при переносе данных : ' + UpdateException.Message);
@@ -4859,30 +4857,26 @@ begin
         end;
       finally
         oldMySqlDB.Close;
-{
         WriteExchangeLog('UpdateToNewLibMySqlDWithGlobalParams', 'Ждем после закрытия соединения');
         Sleep(5000);
-}
       end;
     finally
       oldMySqlDB.Free;
-{
       WriteExchangeLog('UpdateToNewLibMySqlDWithGlobalParams', 'Ждем после закрытия соединения');
       Sleep(5000);
-}
     end;
 
-//    WriteExchangeLog('UpdateToNewLibMySqlDWithGlobalParams', 'Освобождаем старую libd');
+    WriteExchangeLog('UpdateToNewLibMySqlDWithGlobalParams', 'Освобождаем старую libd');
     DatabaseController.FreeMySQLLib('MySql Clients Count при обновлении со старой libd');
 //    WriteExchangeLog('UpdateToNewLibMySqlDWithGlobalParams', 'Освободили старую libd и ждем');
 {$ifdef USEMEMORYCRYPTDLL}
-//    WriteExchangeLog('UpdateToNewLibMySqlDWithGlobalParams', 'Переключаемся на новую libd');
+    WriteExchangeLog('UpdateToNewLibMySqlDWithGlobalParams', 'Переключаемся на новую libd');
     DatabaseController.SwitchMemoryLib();
     //WriteExchangeLog('UpdateToNewLibMySqlDWithGlobalParams', 'Переключились на новую libd и ждем');
     //Sleep(5000);
 {$endif}
 
-//    WriteExchangeLog('UpdateToNewLibMySqlDWithGlobalParams', 'Пытаемся обновить структуру базы данных');
+    WriteExchangeLog('UpdateToNewLibMySqlDWithGlobalParams', 'Пытаемся обновить структуру базы данных');
     UpdateDBFile(dbCon, DBDirectoryName, OldDBVersion, AOnUpdateDBFileData);
 
     dbCon.Open;
