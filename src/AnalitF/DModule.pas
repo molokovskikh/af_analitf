57f5d244718fc3e7cb6645bb6c775d9bf027cdd7
Morozov Sergey <morozov@analit.net>
2011-10-18 09:30:47 +0400

    При запуске программы происходит отключение appdbhlp.dll.

--- src/AnalitF/DModule.pas
+++ src/AnalitF/DModule.pas
@@ -892,7 +892,7 @@ var
 begin
 {$ifdef USEMEMORYCRYPTDLL}
   //Блокируем использование MemoryLib, т.к. оно не работает
-  //DatabaseController.DisableMemoryLib();
+  DatabaseController.DisableMemoryLib();
 {$endif}
   SetNetworkSettings;
 
