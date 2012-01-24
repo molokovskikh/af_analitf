8c1f229b9971d2421b97d1b0008a3258c2a9305d
Morozov Sergey <morozov@analit.net>
2012-01-24 11:02:32 +0400

    Ошибка #8041 Восстановить функционал форматирования на форме "Описание", поломанное в требовании #7957
    сделано.

--- src/AnalitF/DescriptionFrm.pas
+++ src/AnalitF/DescriptionFrm.pas
@@ -66,88 +66,66 @@ var
   trimText : String;
   FDescriptionForm: TDescriptionForm;
 begin
-  //WriteExchangeLog('ShowDescription', 'Создаем TDescriptionForm');
   FDescriptionForm := TDescriptionForm.Create(nil);
   try
     Name := dataSet.FieldByName('Name').AsString;
     EnglishName := dataSet.FieldByName('EnglishName').AsString;
 
-    //WriteExchangeLog('ShowDescription', 'Создаем визуальные компоненты');
     FDescriptionForm.ControlStyle := FDescriptionForm.ControlStyle - [csParentBackground] + [csOpaque];
     FDescriptionForm.pButton.ControlStyle := FDescriptionForm.pButton.ControlStyle - [csParentBackground] + [csOpaque];
 
     RichEd := FDescriptionForm.RxRichEdit;
 
-    //RichEd := TRxRichEdit.Create(FDescriptionForm);
-    //RichEd.Parent := FDescriptionForm;
-    //RichEd.ControlStyle := RichEd.ControlStyle - [csParentBackground] + [csOpaque];
-    //RichEd.Align := alClient;
     RichEd.OnKeyDown := FDescriptionForm.reDescriptionKeyDown;
     RichEd.ReadOnly := True;
-    //FDescriptionForm.ActiveControl := RichEd;
 
-    //WriteExchangeLog('ShowDescription', 'Выставляем размер формы');
     FDescriptionForm.Width := (Application.MainForm.Width div 3) * 2;
     FDescriptionForm.Height := (Application.MainForm.Height div 3) * 2;
 
-    //WriteExchangeLog('ShowDescription', 'Очищаем RichEdit');
-    //WriteExchangeLog('ShowDescription', 'RichEd.Lines.Count : ' + IntToStr(RichEd.Lines.Count));
-    //RichEd.Clear;
-
-    //WriteExchangeLog('ShowDescription', 'Выставляем Paragraph.Alignment := paCenter');
     RichEd.Paragraph.Alignment := paCenter;
-    //WriteExchangeLog('ShowDescription', 'Добавляем две строки');
     RichEd.Lines.Add(Name + ' (' + EnglishName + ')');
     RichEd.Lines.Add('');
-    //WriteExchangeLog('ShowDescription', 'Выставляем аттрибуты выделенного');
-{
+
     RichEd.SelStart := 0;
     RichEd.SelLength := Length(RichEd.Lines[0]);
     RichEd.SelAttributes.Style := RichEd.SelAttributes.Style + [fsBold];
     RichEd.SelAttributes.Size := RichEd.SelAttributes.Size + 8;
-}
 
-    //WriteExchangeLog('ShowDescription', 'LastSelStart := Length(RichEd.Lines[0]) + 2');
     LastSelStart := Length(RichEd.Lines[0]) + 2;
-
-    //WriteExchangeLog('ShowDescription', 'Заполняем RichEdit');
 
     for I := 0 to High(ColumnsByTitles[0]) do begin
       descField := dataSet.FindField(ColumnsByTitles[0, i]);
       if Assigned(descField) and (descField.AsString <> '') then begin
-        RichEd.Paragraph.Alignment := paLeftJustify;
-        RichEd.Paragraph.FirstIndent := 5;
         RichEd.Lines.Add(ColumnsByTitles[1, i] + ':');
         RichEd.Lines.Add('');
-{
+
         RichEd.SelStart := LastSelStart;
         RichEd.SelLength := 0;
+        RichEd.Paragraph.Alignment := paLeftJustify;
+        RichEd.Paragraph.FirstIndent := 5;
         RichEd.SelLength := Length(ColumnsByTitles[1, i] + ':');
         RichEd.SelAttributes.Style := RichEd.SelAttributes.Style + [fsBold];
         RichEd.SelAttributes.Size := RichEd.SelAttributes.Size + 3;
         LastSelStart := LastSelStart + Length(ColumnsByTitles[1, i] + ':') + 2;
-}
 
         lastCount := RichEd.Lines.Count;
         trimText := Trim(descField.AsString);
         trimText := StringReplace(trimText, #13, ' ', [rfReplaceAll]);
         trimText := StringReplace(trimText, #10, '', [rfReplaceAll]);
         RichEd.Lines.Add(trimText);
-{
+
         for J := lastCount to RichEd.Lines.Count-1 do begin
           RichEd.SelStart := LastSelStart;
           RichEd.SelLength := 0;
           RichEd.Paragraph.Alignment := paLeftJustify;
           LastSelStart := LastSelStart + Length(RichEd.Lines[j]);
         end;
-}
 
         RichEd.Lines.Add('');
         LastSelStart := LastSelStart + 2;
       end;
     end;
 
-    //WriteExchangeLog('ShowDescription', 'Удаляем лишние строки из RichEdit');
     for I := RichEd.Lines.Count-1 downto 0 do
       if Trim(RichEd.Lines[i]) = '' then
         RichEd.Lines.Delete(i)
@@ -157,10 +135,8 @@ begin
     RichEd.SelStart := 0;
     RichEd.SelLength := 0;
 
-    //WriteExchangeLog('ShowDescription', 'Отображаем форму');
     FDescriptionForm.ShowModal;
   finally
-    //WriteExchangeLog('ShowDescription', 'Удаляем форму');
     FDescriptionForm.Free;
   end;
 end;
