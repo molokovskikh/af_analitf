c1495dfa5682e242ec1002fafee66f146950793b
Morozov Sergey <morozov@analit.net>
2012-02-10 18:09:41 +0400

    Собрал релиз версии 7.7.3.1719.
    Релиз упакован VMProtect.
    зашифровал appdbhlp.dll новым ключом и пересобрал релиз.
    Релиз от 1711 отличается только тем, что имеет другую специализарованную сборку libmysqld.dll.
    Добавил файлы для обновления программы с версии 1611-1651 до версии 1719.

--- src/AnalitF/DModule.pas
+++ src/AnalitF/DModule.pas
@@ -3654,7 +3654,7 @@ begin
     then
       raise Exception.Create('Библиотека libmysqld.dll повреждена.');
     calchash := GetFileHash(ExePath + LibraryFileNameStart + LibraryFileNameEnd);
-    if AnsiCompareText(calchash, 'DDB492A538E1289E47D158F35AFFAD43') <> 0 then
+    if AnsiCompareText(calchash, '22473A4A8C728C5A6844471B068928EB') <> 0 then
       raise Exception.Create('Невозможно загрузить библиотеку libmysqld.dll.');
   except
     on E : Exception do begin
